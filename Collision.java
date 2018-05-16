6794a91e128eab9caee00b9af5b5f95f27fb0dec
Anton Blåberg <35953578+AntonBlaberg@users.noreply.github.com>
2018-05-16 15:00:46 +0200

    Add files via upload

--- Collision.java
+++ Collision.java
@@ -1,4 +1,4 @@
-package Studs_boll_med_boll;	// Ta bort denna om ni inte har ett sånt paket (inga problem kan hända om ni tar bort)
+package Studs_boll_med_boll;
 
 public class Collision {
 	
@@ -10,22 +10,27 @@ public class Collision {
 		double P2VxSqr = P2.getVx() * P2.getVx();
 		double P2VySqr = P2.getVy() * P2.getVy();
 		
-			// Gör om partiklarnas x och y hastigheter till vektorer med "längd" och vinkel
+			//hastighetsvektorer för båda bollarna
 		   double speed1 = Math.sqrt(P1VxSqr + P1VySqr);
 		   double speed2 = Math.sqrt(P2VxSqr + P2VySqr);
 		   double direction_1 = Math.atan2(P1.getVy(), P1.getVx());
 		   double direction_2 = Math.atan2(P2.getVy(), P2.getVx());
+		   
+		   //Hastigheter boll mot boll, och vinkelrätt boll mot boll. (I ett system med X och Y)
 		   double new_xspeed_1 = speed1 * Math.cos(direction_1 - angle);
 		   double new_yspeed_1 = speed1 * Math.sin(direction_1 - angle);
 		   double new_xspeed_2 = speed2 * Math.cos(direction_2 - angle);
 		   double new_yspeed_2 = speed2 * Math.sin(direction_2 - angle);
 
-		   //Slutspeed
+		   //X och Y axlar är här vridna till boll mot boll och vinkelrätt boll mot boll.
+		   //Final X hastigheter uträknas genom elastisk krock med massa inräknat. 
+		   //Final Y hastigheter händer ingenting med.
 		   double final_xspeed_1 = ((P1.getM() - P2.getM()) * new_xspeed_1 + (P2.getM() + P2.getM()) * new_xspeed_2) / (P1.getM() + P2.getM());
 		   double final_xspeed_2 = ((P1.getM() + P1.getM()) * new_xspeed_1 + (P2.getM() - P1.getM()) * new_xspeed_2) / (P1.getM() + P2.getM());
 		   double final_yspeed_1 = new_yspeed_1;
 		   double final_yspeed_2 = new_yspeed_2;
 
+		   //Mindre kod
 		   double cosAngle = Math.cos(angle);
 		   double sinAngle = Math.sin(angle);
 		   
